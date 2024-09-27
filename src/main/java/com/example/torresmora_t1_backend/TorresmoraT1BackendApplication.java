9148eaf53d01292d5b64c78dfdfdf3695f0f289c
Jorge Enrique Torres Mora <jorgetorresmora99@gmail.com>
2024-09-26 23:11:01 -0500

    Primer commit

---
+++ src/main/java/com/example/torresmora_t1_backend/TorresmoraT1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.torresmora_t1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorresmoraT1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorresmoraT1BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/torresmora_t1_backend/TorresmoraT1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.torresmora_t1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorresmoraT1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
